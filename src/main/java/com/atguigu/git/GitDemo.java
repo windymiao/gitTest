f12d9681dd93a3e84095893a1d4d68cd78988341
windy <2267324648@qq.com>
2022-05-18 20:19:18 +0800

    idea commit hot-fix test

--- src/main/java/com/atguigu/git/GitDemo.java
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hot-fix");
     }
 }
