2334a58f80800dcd375990543133230a841c4783
windy <2267324648@qq.com>
2022-05-18 20:17:22 +0800

    idea commit test1

---
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("2022/5/18");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
